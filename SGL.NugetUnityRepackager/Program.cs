d84cd1c17846acc53beb1823dcbd7179dbec25b0
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-07-04 18:07:18 +0200

    Implemented basic test code to check working with authenicated and mapped repositories.

--- SGL.NugetUnityRepackager/Program.cs
+++ SGL.NugetUnityRepackager/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace SGL.NugetUnityRepackager
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+﻿using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace SGL.NugetUnityRepackager {
+	internal class Program {
+		static async Task Main(string[] args) {
+			ILogger logger = NullLogger.Instance;
+			CancellationToken ct = CancellationToken.None;
+
+			SourceCacheContext cache = new SourceCacheContext();
+			var settings = Settings.LoadDefaultSettings(Directory.GetCurrentDirectory());
+			var packageSources = new PackageSourceProvider(settings).LoadPackageSources().ToDictionary(ps => ps.Name);
+			var packageSourcesMapping = PackageSourceMapping.GetPackageSourceMapping(settings);
+
+			var packageName = "SGL.Utilities";
+			var sourceNames = packageSourcesMapping.GetConfiguredPackageSources(packageName);
+			var repository = Repository.Factory.GetCoreV3(packageSources[sourceNames.First()]);
+			FindPackageByIdResource resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+			IEnumerable<NuGetVersion> versions = await resource.GetAllVersionsAsync(packageName, cache, logger, ct);
+
+			foreach (NuGetVersion version in versions) {
+				Console.WriteLine($"Found version {version}");
+			}
+		}
+	}
 }
