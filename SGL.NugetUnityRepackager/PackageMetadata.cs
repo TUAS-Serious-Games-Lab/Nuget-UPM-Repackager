932fb6a4cd0a50d15c124082fab6321cdd7447f9
Stefan Bodenschatz <S.Bodenschatz@hochschule-trier.de>
2022-07-12 18:24:25 +0200

    Specified PackageMetadata.

--- SGL.NugetUnityRepackager/PackageMetadata.cs
+++ SGL.NugetUnityRepackager/PackageMetadata.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NuGet.Packaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace SGL.NugetUnityRepackager {
 	public class PackageMetadata {
-		// Add needed properties
-		public IReadOnlyDictionary<string, string> OtherMetadata { get; }
+		public string Title { get; init; }
+		public string Authors { get; init; }
+		public string Tags { get; init; }
+		public string Owners { get; init; }
+		public string Description { get; init; }
+		public string ReleaseNotes { get; init; }
+		public string Summary { get; init; }
+		public string ProjectUrl { get; init; }
+		public string Icon { get; init; }
+		public string IconUrl { get; init; }
+		public string Copyright { get; init; }
+		public string Readme { get; init; }
+		public string LicenseUrl { get; init; }
+		public LicenseMetadata LicenseMetadata { get; init; }
+		public bool RequireLicenseAcceptance { get; init; }
+		public IReadOnlyDictionary<string, string> MetadataDictionary { get; init; }
 	}
 }
